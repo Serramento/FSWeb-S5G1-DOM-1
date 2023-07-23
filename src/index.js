ce6cd21ef066f4df12ebdf8c857beaa361aaa2b8
Serramento <134259414+Serramento@users.noreply.github.com>
2023-07-23 12:15:34 +0300

    S5G1 tamamlandı

--- src/index.js
+++ src/index.js
@@ -43,3 +43,59 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+const navItems = document.querySelectorAll("nav a");
+/*navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+navItems[0].setAttribute("class", "italic");
+navItems[1].setAttribute("class", "italic");
+navItems[2].setAttribute("class", "italic");
+navItems[3].setAttribute("class", "italic");
+navItems[4].setAttribute("class", "italic");
+navItems[5].setAttribute("class", "italic");*/
+
+navItems.forEach((item , index) => {
+  let sira = index + 1
+  item.textContent = siteContent.nav["nav-item-" + sira]
+  item.setAttribute("class", "italic")
+})
+
+const logo = document.querySelector("#logo-img")
+
+//logo.setAttribute("src", siteContent.images["logo-img"])
+
+logo.src = siteContent.images["logo-img"]
+
+const ctaH1 = document.querySelector(".cta h1")
+ctaH1.textContent = siteContent.cta.h1
+const ctaButton = document.querySelector(".cta button")
+ctaButton.textContent = siteContent.cta.button
+//const ctaImg = document.querySelector(".cta img")
+const ctaImg = document.getElementById("cta-img")
+ctaImg.setAttribute("src",siteContent.images["cta-img"])
+
+let textBaslik = document.querySelectorAll(".text-content h4")
+let textP = document.querySelectorAll(".text-content p")
+
+for(let i = 0; i<5; i++){
+  textBaslik[i].textContent = Object.values(siteContent["ana-içerik"])[i*2];
+  textP[i].textContent= Object.values(siteContent["ana-içerik"])[i*2+1];
+}
+
+document.querySelector(".middle-img").src = siteContent.images["accent-img"]
+
+document.querySelector(".contact h4").textContent = siteContent.iletisim["iletişim-h4"]
+let iletisimP = document.querySelectorAll(".contact p")
+iletisimP[0].textContent = siteContent.iletisim.adres
+iletisimP[1].textContent = siteContent.iletisim.telefon
+iletisimP[2].textContent = siteContent.iletisim.email
+
+let footerA = document.querySelector("footer a")
+footerA.textContent = siteContent.footer.copyright
+//footerA.setAttribute("class", "bold")
+footerA.classList.add("bold")
